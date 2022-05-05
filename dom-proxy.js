62ca1cc294afb88d4288b7df466588cd5097c113
Gregory Propf <gpropf@gmail.com>
2022-05-04 18:58:13 -0700

    Lot's of progress on C->JS.

--- dom-proxy.js
+++ dom-proxy.js
@@ -7,8 +7,10 @@ function gv(element_name, attribute_name) {
 
 class CLElement {
     constructor() {
+        
         this._type = "";
         this._id = "";
+        this._dom_element = null;
     }
 
     init(type, id) {
@@ -20,8 +22,30 @@ class CLElement {
         return new CLElement();
     }
 
-    get_type() {
+    setval(v) {
+        
+    }
+
+    get type() {
         return this._type;
+    }
+
+    set type(type) {
+        this._type = type;
+    }
+
+    set id(id) {
+        this._id = id;
+        var el = document.getElementById(this._id);
+        this._dom_element = el;
+        this._dom_element.onchange = function() {
+            alert(el.value);
+        };
+    }
+
+
+    set value(v) {
+        this._dom_element.value = v;
     }
 }
 
--- hello_world.cpp
+++ hello_world.cpp
@@ -8,14 +8,45 @@
 #include <stdio.h>
 #include <iostream>
 #include <emscripten/val.h>
+#include <emscripten/bind.h>
 
 
 using namespace std;
 using namespace emscripten;
 
+
+class CLElement_CPP {
+  public:
+    CLElement_CPP(string type, string id): _type(type), _id(id) {
+
+    }
+
+    string getType() const { return _type; }
+    void setType(string type) { _type = type; }
+    string getId() const { return _id; }
+    void setId(string id) { _id = id; }
+
+
+    private:
+      std::string _type;
+      std::string _id;
+};
+
+EMSCRIPTEN_BINDINGS(my_class_example) {
+  class_<CLElement_CPP>("CLElement_CPP")
+    .constructor<std::string, std::string>()
+    //.function("incrementX", &MyClass::incrementX)
+    .property("type", &CLElement_CPP::getType, &CLElement_CPP::setType)
+    .property("id", &CLElement_CPP::getId, &CLElement_CPP::setId)
+    //.class_function("getStringFromInstance", &CLElement_CPP::getStringFromInstance)
+    ;
+}
+
+
+
 int main() {
-  val AudioContext = val::global("CLElement");
-  if (AudioContext.as<bool>()) {
+  val CLContext = val::global("CLElement");
+  if (CLContext.as<bool>()) {
     printf("Got CLElement!\n");
     
   }
@@ -23,26 +54,21 @@ int main() {
     return -1;
   }
 
-  printf("Got an AudioContext\n");
-  val context = AudioContext.new_();
-  val oscillator = context.call<val>("createCLElement");
+  printf("Got a CLContext\n");
+  val context = CLContext.new_();
+  val test_input = context.call<val>("createCLElement");  
 
+  printf("Configuring testinput\n");
+  test_input.set("type", val("text"));
+  test_input.set("id", val("testinput"));  
   
-  
-
-  printf("Configuring oscillator\n");
-  oscillator.set("_type", val("triangle"));
-  oscillator.set("_id", val("tf1"));
-  
-  //return 0;
-  
-  //oscillator["frequency"].set("value", val(261.63)); // Middle C
-
   printf("Playing\n");
-  string tstr = oscillator.call<string>("get_type");
-  //oscillator.call<void>("start", 0);
 
-  printf("tstr: %s, All done!\n", tstr.c_str());
+  test_input.set("value", val(101));
+  //string tstr = test_input.call<string>("get_type");
+  
+  //printf("tstr: %s, All done!\n", tstr.c_str());
 
   return 0;
 }
+
