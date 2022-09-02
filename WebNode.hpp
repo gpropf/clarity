5e8c73bf7921832659fa0edb9911ee0d2f4a821e
Greg Propf <gpropf@gmail.com>
2022-09-02 12:09:50 -0700

    Cleanup of WebNode.hpp

--- WebNode.hpp
+++ WebNode.hpp
@@ -7,18 +7,11 @@
 
 namespace clarity
 {
-
   class WebNode : public ControlNetworkNode
   {
   public:
     WebNode() : ControlNetworkNode() {}
-    WebNode(const string &name, const CppType storedValueType) : ControlNetworkNode(name, storedValueType)
-    {
-
-      // cout<< "DTEST:WebNode(const CppType storedValueType):" << (int)storedValueType << " id = " << id_ << "\n";
-    }
-
-    
+    WebNode(const string &name, const CppType storedValueType) : ControlNetworkNode(name, storedValueType) {}
 
     template <typename T>
     void installModelNode(T *dynval)
@@ -29,50 +22,31 @@ namespace clarity
       m->pushValToPeersThruAL(m);
     }
 
-    // template <typename T>
-    // ModelNode<T> * getInnerModelNode() {
-
-    // }
-
     val getVal() const
     {
-      // ControlNetworkNode::getVal();
-      
       val domElement = cle_["domElement"];
       string valueText = domElement[boundField_].as<string>();
 
       switch (this->storedValueType_)
       {
       case CppType::Int:
-        // cout<< "GETVAL Int: " << valueText << "\n";
         return val(stoi(valueText));
         break;
       case CppType::Float:
-        // cout<< "GETVAL Float: " << valueText << "\n";
         return val(stof(valueText));
         break;
       case CppType::Double:
-        // cout<< "GETVAL Double: " << valueText << "\n";
         return val(stod(valueText));
         break;
       case CppType::String:
-        // cout<< "GETVAL String: " << valueText << "\n";
         return val(valueText);
         break;
       case CppType::NoData:
-        // cout<< "GETVAL NoData\n";
       default:
         return val(NULL);
         break;
       }
     }
-
-  protected:
-    // inline void setAsProxyFor(clarity::WebNode *proxyiedElement)
-    // {
-    //   boundField_ = proxyiedElement->boundField_;
-    // }
-    
 
   public:
     string boundField_;
@@ -82,8 +56,7 @@ namespace clarity
       val domElement = cle_["domElement"];
       domElement.set(boundField_, inval);
     }
-
-    // }
   };
 }
+
 #endif
