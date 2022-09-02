8cc8452877a7adcfffbad6ed21e455bde40dcd81
Greg Propf <gpropf@gmail.com>
2022-09-02 12:05:44 -0700

    Moved implementation methods out of WebElemNode.hpp

--- WebElemNode.cpp
+++ WebElemNode.cpp
@@ -5,7 +5,7 @@ using namespace clarity;
 
 WebElemNode::WebElemNode(const string &name, const string &tag,
                          const CppType storedValueType) : WebNode(name, storedValueType)
-{    
+{
     cle_.call<void>("createDOMElement", id_, tag, storedValueType, name);
     cle_.set("name", val(name));
     tag_ = tag;
@@ -13,5 +13,27 @@ WebElemNode::WebElemNode(const string &name, const string &tag,
     boundField_ = "value";
     ControlNetworkNode::switchboard[id_] = this;
 }
+
+inline void WebElemNode::setStoredValueType(CppType cppType)
+{
+    storedValueType_ = cppType;
+    cle_.set("cpptype", cppType);
+}
+
+inline void WebElemNode::setAttribute(const string &attr, const val &value)
+{
+    val domElement = cle_["domElement"];
+    domElement.call<void>("setAttribute", attr, value);
+}
+
+inline void WebElemNode::addEventListenerByName(const string &eventName, const string &callbackName)
+    {
+      cle_.call<void>("addEventListenerById", eventName, callbackName);
+    }
+
+    inline void WebElemNode::addJSEventListener(const string &eventName, val eventCallback)
+    {
+      cle_.call<void>("addEventListener", eventName, eventCallback);
+    }
 
 static map<string, std::function<void()>> callbackMap;
--- WebElemNode.hpp
+++ WebElemNode.hpp
@@ -6,10 +6,8 @@
 
 namespace clarity
 {
-
   class WebElemNode : public WebNode
   {
-  public:
   protected:
     string tag_;
     string stringId_;
@@ -29,35 +27,17 @@ namespace clarity
 
     static map<string, std::function<void()>> callbackMap;
 
-    inline void setAttribute(const string &attr, const val &value)
-    {
-      val domElement = cle_["domElement"];
-      domElement.call<void>("setAttribute", attr, value);
-    };
+    EMSCRIPTEN_KEEPALIVE inline void setAttribute(const string &attr, const val &value);    
 
-    inline void addEventListenerByName(const string &eventName, const string &callbackName)
-    {
-      cle_.call<void>("addEventListenerById", eventName, callbackName);
-    }
-
-    inline void addJSEventListener(const string &eventName, val eventCallback)
-    {
-      cle_.call<void>("addEventListener", eventName, eventCallback);
-    }
+    EMSCRIPTEN_KEEPALIVE void addEventListenerByName(const string &eventName, const string &callbackName);
+    EMSCRIPTEN_KEEPALIVE void addJSEventListener(const string &eventName, val eventCallback);
 
     inline string getTag() const { return tag_; }
-
     inline int getId() const { return id_; }
-
     inline void setJsval(val jsval) { cle_ = jsval; }
 
     inline CppType getStoredValueType() const { return storedValueType_; }
-
-    inline void setStoredValueType(CppType cppType)
-    {
-      storedValueType_ = cppType;
-      cle_.set("cpptype", cppType);
-    }
+    EMSCRIPTEN_KEEPALIVE void setStoredValueType(CppType cppType);
 
     inline static void runCallbackById(const string &id) { callbackMap[id](); }
   };
