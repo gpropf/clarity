65c8bf265b4463aa261b038ccff57eb9ea3b27cc
Greg Propf <gpropf@gmail.com>
2022-09-04 12:52:54 -0700

    Updated emsdk to 3.1.20 and started cutting away non-essential stuff in testbed.

--- Makefile
+++ Makefile
@@ -1,18 +1,12 @@
 
 ENV		= DEMANGLE_SUPPORT=1 EMCC_DEBUG=1 TOTAL_MEMORY=1900mb
-#EMCC_DEBUG=1 TOTAL_MEMORY=1900mb
-# -gsource-map --source-map-base .
-# --source-map-base=http://127.0.0.1
+
 CC		= emcc
 CFLAGS	=  -O0 -g -gsource-map --source-map-base=http://localhost:6931/ -std=c++17
-#CFLAGS	=  -std=c++17 -g
-# -gsource-map
-# --source-map-base smap_ -gseparate-dwarf
-JSOUT	= clarity_embind.js
-NUKE_HTML_OUT = nuke.html
-NUKE_JS_OUT = nuke.js
 
-# CPPIN	= clarity.cpp ButtonElement.cpp WebElemNode.cpp nuke.cpp
+JSOUT	= clarity_embind.js
+
+
 LIBCLR_IN = WebElemNode.cpp WebAttrNode.cpp \
 			ControlNetworkNode.cpp CompoundElement.cpp LabelledInput.cpp \
 			LabelElement.cpp CanvasElement.cpp
--- testbed.cpp
+++ testbed.cpp
@@ -14,41 +14,41 @@ val ControlNetworkNode::ActiveLink::CLElement_ = val::global("CLElement");
 int main() {
     using ActiveLink = ControlNetworkNode::ActiveLink;
 
-    double *n = new double(50);
-    double *pi = new double(3.14159);
+    // double *n = new double(50);
+    // double *pi = new double(3.14159);
     double *a = new double(2.78);
 
     WebElemNode *maindiv = new WebElemNode("maindiv", "div", CppType::NoData);
 
     CLNodeFactory<WebElemNode, double> builder("input", "input_double",
-                                               CppType::Double, pi);
+                                               CppType::Double, a);
 
-    WebElemNode *input1 = builder.buildWithModelNode();
-    WebElemNode *input2 = builder.withStoredValue(n).buildWithModelNode();
-    WebElemNode *button1 = builder.button("button1", "Press me!");
-    WebElemNode *labelInput1 = builder.label(input1, "PI");
-    button1->addEventListenerByName("click", "iterateModel");
+    // WebElemNode *input1 = builder.buildWithModelNode();
+    //  WebElemNode *input2 = builder.withStoredValue(n).buildWithModelNode();
+    //  WebElemNode *button1 = builder.button("button1", "Press me!");
+    //  WebElemNode *labelInput1 = builder.label(input1, "PI");
+    //  button1->addEventListenerByName("click", "iterateModel");
 
     map<string, val> inputFieldAttrs = {{"type", val("text")}};
     WebElemNode *input_a = builder.withStoredValue(a)
                                .withName("input_a_text")
                                .withAttributes(inputFieldAttrs)
                                .buildWithModelNode();
-    WebElemNode *labelled_input_a =
-        builder.labelGivenNode(input_a, "Label for A");
+    // WebElemNode *labelled_input_a =
+    //     builder.labelGivenNode(input_a, "Label for A");
 
-    maindiv->appendChild(input1);
-    maindiv->appendChild(labelInput1);
-    maindiv->appendChild(input2);
-    maindiv->appendChild(button1);
-    // maindiv->appendChild(input_a);
-    maindiv->appendChild(labelled_input_a);
-    input1->setAttribute("type", val("text"));
-    input2->setAttribute("type", val("text"));
+    // maindiv->appendChild(input1);
+    //  maindiv->appendChild(labelInput1);
+    //  maindiv->appendChild(input2);
+    //  maindiv->appendChild(button1);
+    maindiv->appendChild(input_a);
+    // maindiv->appendChild(labelled_input_a);
+    // input1->setAttribute("type", val("text"));
+    // input2->setAttribute("type", val("text"));
 
-    WebElemNode::callbackMap["iterateModel"] = [=] {
-        cout << "callbackMap[\"iterateModel\"]\n";
-    };
+    // WebElemNode::callbackMap["iterateModel"] = [=] {
+    //     cout << "callbackMap[\"iterateModel\"]\n";
+    // };
 
     printf("Setup complete!\n");
     return 0;
