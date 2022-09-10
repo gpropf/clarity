07ef83e80f6324538a767b26022159cc6e8e162b
Greg Propf <gpropf@gmail.com>
2022-09-10 12:31:59 -0700

    Got rid of a lot of commented out cruft.

--- testbed.cpp
+++ testbed.cpp
@@ -19,17 +19,10 @@ int main() {
     val doNothing = CLE["doNothing"];
     val square = CLE["square"];
     val blackbody = CLE["blackbody"];
-
-    // double *n = new double(50);
-    // double *pi = new double(3.14159);
+  
     double *a = new double(27.8);
     ModelNode<double> *a_mn =
         new ModelNode(a, CppType::Double, "independently_created_modelnode");
-    // CLNodeFactory<ControlNetworkNode, double> builder("div", "maindiv",
-    //                                                   CppType::NoData, a);
-
-    // ControlNetworkNode *maindiv =
-    //     new ControlNetworkNode("maindiv", "div", CppType::NoData);
 
     CLNodeFactory<ClarityNode, double> builder("div", "maindiv",
                                                CppType::NoData);
@@ -49,18 +42,7 @@ int main() {
 
     ClarityNode *input_a = inputBuilder.withName("input_a_text")
                                .withModelNode(a_mn)
-                               // .withTransformFn(square)
-                               //.withLinkMultiplierConstant(0.1)
-                               //.withLinkMultiplierConstant(3)
-                               //.withStoredValueType(CppType::Double)
-
-                               // .extractModelNode(a_mn)
-                               // .withTag("input")
-                               //  .withAttributes(inputFieldAttrs)
-                               .build();
-
-    // cout << "ModelNode should now have been extracted.\n"
-    //      << input_a->countPeers() << "\n";
+                               .build();   
 
     ClarityNode *svgarea =
         childOfMaindivBuilder.withName("svgarea")
@@ -96,72 +78,18 @@ int main() {
                                   .withTransformFns(blackbody)
                                   .withAttributes({})
                                   .attributeNode("fill", cir1);
-
-    // cout << "attributeNode should now have been created.\n"
-    //      << a_mn->nodeStats() << "\n";
-
+  
     ClarityNode *range_a = inputBuilder
                                .withModelNode(a_mn)
-                               //.withStoredValueType(CppType::Double)
                                .withName("range_a")
-                               .withAttributes({{"type", val("range")}})
-                               // .extractModelNode(a_mn)
-                               // .withTag("input")
-                               //  .withAttributes(inputFieldAttrs)
+                               .withAttributes({{"type", val("range")}})                               
                                .build();
 
     ClarityNode::callbackMap["printStats"] = [=] {
         cout << "callbackMap[\"iterateModel\"]\n";
     };
 
-    // statusButton->addEventListenerByName("onclick", "printStats");
-
-    // ControlNetworkNode *cir1Radius =
-    // childOfMaindivBuilder.withName("cir1Radius")
-    //         .withParent(cir1).withBoundField("r").build();
-
-    //         cir1->setAttribute();
-    // cir1->setAttribute();
-    // cir1->setAttribute();
-    // cir1->setAttribute("r", val(80));
-
-    // svgarea->appendChild(cir1);
-
-    //     new clarity::WebElemNode("svgarea", "svg", clarity::CppType::NoData);
-    // clarity::WebElemNode *cir1 =
-    //     new clarity::WebElemNode("cir1", "circle", clarity::CppType::Double);
-    // clarity::WebAttrNode *cir1Radius =
-    //     new clarity::WebAttrNode("r", clarity::CppType::Double, cir1);
-
-    // ControlNetworkNode *input_a =
-    //     new ControlNetworkNode("input_a_text", "input", CppType::Double);
-
-    // ModelNode<double> *mn = new ModelNode(a, CppType::Double);
-    // mn->addPeer(ActiveLink(input_a, 1));
-    // mn->pushValToPeers(mn);
-
-    // cout << "CPPType for input_a is: " << (int)input_a->getStoredValueType()
-    //      << "\n";
-
-    // WebElemNode *labelled_input_a =
-    //     builder.labelGivenNode(input_a, "Label for A");
-
-    // maindiv->appendChild(input1);
-    //  maindiv->appendChild(labelInput1);
-    //  maindiv->appendChild(input2);
-    //  maindiv->appendChild(button1);
-    // maindiv->appendChild(svgarea);
-    // maindiv->appendChild(input_a);
-
-    // maindiv->appendChild(labelled_input_a);
-    // input1->setAttribute("type", val("text"));
-    // input2->setAttribute("type", val("text"));
-
-    // WebElemNode::callbackMap["iterateModel"] = [=] {
-    //     cout << "callbackMap[\"iterateModel\"]\n";
-    // };
-
-    printf("Setup complete!\n");
+      printf("Setup complete!\n");
 
     return 0;
 }
