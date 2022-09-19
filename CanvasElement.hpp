38a867d1fee51bfe7d51c4b5f3ff9c6f54dba4a2
Greg Propf <gpropf@gmail.com>
2022-09-18 20:57:05 -0700

    Removed some cruft in CanvasElement class.

--- CanvasElement.hpp
+++ CanvasElement.hpp
@@ -5,27 +5,11 @@
 
 namespace clarity {
 
-class CanvasElement : public ClarityNode
-
-{
+class CanvasElement : public ClarityNode {
    public:
-    CanvasElement() : ClarityNode() {
-        cout << "NO ARGS: CanvasElement!! CanvasElement!! CanvasElement!! CanvasElement!! CanvasElement!! CanvasElement!! \n";
-    }
-
-    CanvasElement(const string &drawFuntionName);
-
     CanvasElement(const string &name, const string &tag,
                   const CppType storedValueType, bool useExistingDOMElement)
-        : ClarityNode(name, tag, storedValueType, useExistingDOMElement) {
-        cle_.call<void>("drawBlueTestPattern");
-        cout << "CanvasElement!! CanvasElement!! CanvasElement!! CanvasElement!! CanvasElement!! CanvasElement!! \n";
-    }
-
-    // CanvasElement(int width, int height, const string &name = "",
-    //               const string &drawFuntionName = "");
-
-    // CanvasElement(const string &drawFuntionName);
+        : ClarityNode(name, tag, storedValueType, useExistingDOMElement) {}
 
     inline void setDrawFuntionName(const string &drawFuntionName) {
         drawFuntionName_ = drawFuntionName;
