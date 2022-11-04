acbbd93d0ce90d4cc8d08be692319a0b021ffd28
Greg Propf <gpropf@gmail.com>
2022-11-03 19:55:23 -0700

    Added specialization for pair<int, string> in Select.

--- Selectables.cpp
+++ Selectables.cpp
@@ -6,6 +6,14 @@
 // val CLElement_ = val::global("CLElement");
 
 template <>
+string Select<vector<pair<int, string>>>::cppValToString() const {
+    // cout << "HybridNode<string>::cppValToString()\n";
+    if (cppVal_ == nullptr) return "Select<vector<pair<int, string>>> NULLPTR";
+    return string("Number of pair<int, string> in the selection: ") +
+           to_string(cppVal_->size());  // Show the first string in the vector of strings.
+}
+
+template <>
 string Select<vector<string>>::cppValToString() const {
     // cout << "HybridNode<string>::cppValToString()\n";
     if (cppVal_ == nullptr) return "Select<vector<string>> NULLPTR";
