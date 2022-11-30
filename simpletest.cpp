8694092dd95954dec2af2784f1fb682aa57db772
Greg Propf <gpropf@gmail.com>
2022-11-29 19:55:13 -0800

    Makefile fixes to use web workers. Still not working.

--- simpletest.cpp
+++ simpletest.cpp
@@ -19,8 +19,8 @@ void run_in_worker() { printf("Hello from wasm worker!\n"); }
 int main() {
     emscripten_wasm_worker_t worker = emscripten_malloc_wasm_worker(/*stack size: */ 1024);
     emscripten_wasm_worker_post_function_v(worker, run_in_worker);
-    // SimpleTest simpleTest;
-    // TestFramework testFramework;
-    // testFramework.content(simpleTest.content());
+    SimpleTest simpleTest;
+    TestFramework testFramework;
+    testFramework.content(simpleTest.content());
     return 0;
 }
