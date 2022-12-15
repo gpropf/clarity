7cbb7171531a2ed44ad7631242a9045dff790127
Greg Propf <gpropf@gmail.com>
2022-12-15 00:22:15 -0800

    Found and fixed some bogus bindings in embindings.hpp.

--- CLNodeFactory.hpp
+++ CLNodeFactory.hpp
@@ -752,7 +752,7 @@ class CLNodeFactory {
         }
 //this.clarityNode_.nodelog
         val valueIncrementerEL =
-            button->getCLE().template call<val>("generateValueModifierEL", val(button->getCLE()), val(1));
+            button->getCLE().template call<val>("generateValueModifierEL", val(cyclicVal), val(1));
         val buttonDOMElement = button->getCLE()["domElement"];
         buttonDOMElement.call<void>("addEventListener", val("click"), valueIncrementerEL);
         return button;
