bb1fdc7a41357ec9808ba6a5a8b76acfb2090b7d
Greg Propf <gpropf@gmail.com>
2023-01-20 21:44:17 -0800

    Added lots of files from main clarity dir to the CL2 dir since some of that stuff may need to diverge.

--- CL2/WebElementBuilder.hpp
+++ CL2/WebElementBuilder.hpp
@@ -1,6 +1,26 @@
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <functional>
+#include <iostream>
+#include <map>
+#include <sstream>
+
 
 namespace cl2 {
 
-struct WebElementBuilder {};
+    
+
+class WebElementBuilder {;
+    std::string tag_, name_;
+    int id_;
+    public:
+    WebElementBuilder(std::string tag, std::string name, int id): tag_(tag), name_(name), id_(id) {
+        std::cout << "WebElementBuilder CALLED!" << std::endl;
+    }
+};
 
 }  // namespace cl2
---
+++ CL2/signalTest.cpp
@@ -0,0 +1,12 @@
+
+#include "WebElementBuilder.hpp"
+
+
+
+int main() {
+    std::cout << "HELLO from signalTest.cpp!" << std::endl;
+    cl2::WebElementBuilder web("input", "webel", 1);
+
+
+    return 0;
+}
