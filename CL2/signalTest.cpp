6c21a2d319933ee03285c6e25bd0bcc6d6f30430
Greg Propf <gpropf@gmail.com>
2023-01-22 22:00:54 -0800

    Ok I think I see the problem. Need to put everything on the heap to prevent premature dealloc.

--- CL2/signalTest.cpp
+++ CL2/signalTest.cpp
@@ -10,8 +10,8 @@ int main() {
     cl2::ConsoleLoggerSignalObject<std::string> clso(logFn);
     ssWeb.addOutput(&clso);
     std::string testStr = "Foo string";
-    shared_ptr<std::string> testStrPtr = make_shared<std::string>(testStr);
-    shared_ptr<void> testStrPtrVoid = std::static_pointer_cast<void>(testStrPtr);
+    // shared_ptr<std::string> testStrPtr = make_shared<std::string>(testStr);
+    // shared_ptr<void> testStrPtrVoid = std::static_pointer_cast<void>(testStrPtr);
     // ssWeb.emit(testStrPtrVoid);
     ssWeb.emitOne(&clso, testStr);
     ssWeb.finalize();
