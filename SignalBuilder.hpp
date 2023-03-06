546f652a9e2fd09253de67fd3f4c3230fe8a8f55
Greg Propf <gpropf@gmail.com>
2023-03-05 16:06:20 -0800

    Added docstrings for SignalBuilder.hpp

--- SignalBuilder.hpp
+++ SignalBuilder.hpp
@@ -4,11 +4,10 @@
  * @brief This is the factory class that creates and then connects the signals.
  * @version 0.1
  * @date 2023-03-05
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
-
 
 #ifndef SignalBuilder_hpp
 #define SignalBuilder_hpp
@@ -20,13 +19,19 @@
 
 namespace cl2 {
 
+/**
+ * @brief Factory class that creates WebElements and signals. Also connects signals to one another.
+ *
+ */
 class SignalBuilder {
     TicketMachine tm_;
     bool labelAllInputs_ = true;
     bool labelsSwallowTheirReferents_ = true;
-    bool addBRAfterAllCalls_ = true;
+    bool addBRAfterAllCalls_ =
+        true;  //!< add a '<BR>' tag after each call so everything goes on its own line.
     val parentDOMElement_ = val::null();
-    std::map<const std::string, const WebElement*> elementMap_;
+    std::map<const std::string, const WebElement*>
+        elementMap_;  //!< a map of elements we've created so we can re-use them.
 
    public:
     SignalBuilder(int startId = 0, bool labelAllInputs = true,
@@ -37,17 +42,30 @@ class SignalBuilder {
         tm_ = TicketMachine(startId);
     }
 
+    /**
+     * @brief Do this after each method call.
+     *
+     */
     void postCall() {
         if (addBRAfterAllCalls_) BR(this->parentDOMElement_);
     }
 
+    /**
+     * @brief Add a WebElement to the global map.
+     *
+     * @param wel
+     * @param name
+     */
     void addElementToMap(const WebElement& wel, const std::string& name) {
-        // std::string name = "foo";//wel.getName();
-        // std::string name = wel.domElement_.call<val>("getAttribute",
-        // val("name")).as<std::string>(); elementMap_[name] = &wel;
         elementMap_.insert({name, &wel});
     }
 
+    /**
+     * @brief Get an element from the global map. Returns a null pointer if element is not found.
+     *
+     * @param name
+     * @return const WebElement*
+     */
     const WebElement* getElementByName(const std::string& name) {
         if (elementMap_.find(name) != elementMap_.end()) {
             return elementMap_[name];
@@ -55,6 +73,17 @@ class SignalBuilder {
         return nullptr;
     }
 
+    /**
+     * @brief Creates a `WebElementSignalObject` that represents the specified attribute of a
+     * `WebElement`. An example would be creating an attribute signal to represent the radius of an
+     * SVG circle object.
+     *
+     * @tparam S
+     * @param wel
+     * @param attributeName
+     * @param emitInitialValue
+     * @return shared_ptr<WebElementSignalObject<S>>
+     */
     template <typename S>
     shared_ptr<WebElementSignalObject<S>> attributeSignal(const WebElement& wel,
                                                           const std::string& attributeName,
@@ -64,6 +93,16 @@ class SignalBuilder {
         return aso;
     }
 
+    /**
+     * @brief Creates a `WebElementSignalObject` that represents the specified attribute of a
+     * `WebElement` but gets the WE from the global map using the identifier string.
+     *
+     * @tparam S
+     * @param elementName
+     * @param attributeName
+     * @param emitInitialValue
+     * @return shared_ptr<WebElementSignalObject<S>>
+     */
     template <typename S>
     shared_ptr<WebElementSignalObject<S>> attributeSignal(const std::string& elementName,
                                                           const std::string& attributeName,
@@ -75,24 +114,48 @@ class SignalBuilder {
         return nullptr;
     }
 
+    /**
+     * @brief Create future elements as children of the provided DOM element.
+     *
+     * @param parentDOMElement
+     * @return SignalBuilder
+     */
     SignalBuilder withParentDOMElement(val parentDOMElement) const& {
         SignalBuilder cpy(*this);
         cpy.parentDOMElement_ = parentDOMElement;
         return cpy;
     }
 
+    /**
+     * @brief r-value version of above.
+     *
+     * @param parentDOMElement
+     * @return SignalBuilder
+     */
     SignalBuilder withParentDOMElement(val parentDOMElement) && {
         SignalBuilder cpy(std::move(*this));
         cpy.parentDOMElement_ = parentDOMElement;
         return cpy;
     }
 
+    /**
+     * @brief Does what `withParentDOMElement()` does except using a `WebElement` reference.
+     *
+     * @param parentElement
+     * @return SignalBuilder
+     */
     SignalBuilder withParentWebElement(const WebElement& parentElement) const& {
         SignalBuilder cpy(*this);
         cpy.parentDOMElement_ = parentElement.domElement_;
         return cpy;
     }
 
+    /**
+     * @brief r-value version of above.
+     *
+     * @param parentElement
+     * @return SignalBuilder
+     */
     SignalBuilder withParentWebElement(const WebElement& parentElement) && {
         SignalBuilder cpy(std::move(*this));
         cpy.parentDOMElement_ = parentElement.domElement_;
@@ -100,7 +163,7 @@ class SignalBuilder {
     }
 
     /**
-     * @brief Create a text imput field.
+     * @brief Create a text input field.
      *
      * @param name
      * @param labelText
@@ -126,6 +189,15 @@ class SignalBuilder {
     //     return wso;
     // }
 
+    /**
+     * @brief Creates a text input signal emitter using an `EventListenerEmitter`
+     *
+     * @tparam S
+     * @param name
+     * @param labelText
+     * @param emitInitialValue
+     * @return shared_ptr<EventListenerEmitter<S>>
+     */
     template <typename S>
     shared_ptr<EventListenerEmitter<S>> textInputELE(const std::string& name,
                                                      const std::string& labelText,
@@ -142,6 +214,15 @@ class SignalBuilder {
         return wso;
     }
 
+    /**
+     * @brief Creates a "two-sided" text field signal. It can be used as an emitter or acceptor.
+     *
+     * @tparam S
+     * @param name
+     * @param labelText
+     * @param emitInitialValue
+     * @return shared_ptr<WebElementSignalObjectSS<S>>
+     */
     template <typename S>
     shared_ptr<WebElementSignalObjectSS<S>> textInputWSS(const std::string& name,
                                                          const std::string& labelText,
@@ -178,6 +259,15 @@ class SignalBuilder {
     //     return wso;
     // }
 
+    /**
+     * @brief Creates a "two-sided" range field signal. It can be used as an emitter or acceptor.
+     *
+     * @tparam S
+     * @param name
+     * @param labelText
+     * @param emitInitialValue
+     * @return shared_ptr<WebElementSignalObjectSS<S>>
+     */
     template <typename S>
     shared_ptr<WebElementSignalObjectSS<S>> rangeInputWSS(const std::string& name,
                                                           const std::string& labelText,
@@ -188,6 +278,13 @@ class SignalBuilder {
         return wso;
     }
 
+    /**
+     * @brief Create a button with provided callback.
+     *
+     * @param displayedText
+     * @param onClickFn
+     * @return Button
+     */
     Button button(const std::string& displayedText, val onClickFn) {
         Button btn =
             Button(displayedText, displayedText, onClickFn, tm_.getNextStrId(), parentDOMElement_);
@@ -195,6 +292,13 @@ class SignalBuilder {
         return btn;
     }
 
+    /**
+     * @brief Plug an emitter signal into an acceptor.
+     *
+     * @tparam S
+     * @param s1
+     * @param s2
+     */
     template <typename S>
     void connect(shared_ptr<SignalEmitter<S>> s1, shared_ptr<SignalAcceptor<S>> s2) {
         s1->setOutput(s2);
@@ -231,8 +335,8 @@ class SignalBuilder {
     // template <typename S>
     // void connect(shared_ptr<MultiTee<S>> tee, shared_ptr<SignalObject<S>> s1,
     //              shared_ptr<SignalObject<S>> s2, shared_ptr<SignalObject<S>> s3 = nullptr,
-    //              shared_ptr<SignalObject<S>> s4 = nullptr, shared_ptr<SignalObject<S>> s5 = nullptr,
-    //              shared_ptr<SignalObject<S>> s6 = nullptr) {
+    //              shared_ptr<SignalObject<S>> s4 = nullptr, shared_ptr<SignalObject<S>> s5 =
+    //              nullptr, shared_ptr<SignalObject<S>> s6 = nullptr) {
     //     // s1->setOutput(s2);
     //     tee->setOutput(s1);
     //     tee->addOutput(s2);
